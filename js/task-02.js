96befbcde2f7b3e389e4cb1e8c7af829776044ce
Nazar Lyubuska <nazar.lyubuska@gmail.com>
2022-07-25 07:41:13 +0300

    ..

--- js/task-02.js
+++ js/task-02.js
@@ -7,21 +7,6 @@ const ingredients = [
   'Condiments',
 ];
 
-
-// const elements = ingredients.map((ingredient) => {
-//   const element = document.createElement("li");
-//   element.innerHTML = ingredient;
-//   element.classList.add("item");
-//   return element;
-// })
-
-// const elementsList = document.getElementById("ingredients");
-// elementsList.append(...elements);
-// console.log(elementsList);
-
-
-
-
 const elements = ingredients.map((ingredient) => {
   const element = document.createElement("li");
   element.innerHTML = ingredient;
